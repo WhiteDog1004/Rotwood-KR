e63f9af5a73d22d79bf2b0ac69b26977ef92f820
WhiteDog <baek_dog@naver.com>
2024-05-15 18:35:10 +0900

    rw-4 feat : intersection-observer 라이브러리 설치 및 intro 작업

--- src/components/Main/Intro/Intro.styled.ts
+++ src/components/Main/Intro/Intro.styled.ts
@@ -1,12 +1,100 @@
 import styled from "styled-components";
 
 export const IntroContainer = styled.div`
+	position: relative;
 	display: flex;
 	justify-content: center;
-	align-items: center;
+	align-items: flex-start;
 	width: 100%;
 	height: 800px;
 
-	background-color: #666;
+	background-color: #000;
 	color: white;
 `;
+export const IntroTitleBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	font-size: 24px;
+`;
+export const IntroLogo = styled.div`
+	padding: 0 20px;
+	transform: scale(0.8);
+	z-index: 1;
+
+	img {
+		width: 100%;
+		height: 100%;
+	}
+`;
+export const IntroBackground = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	opacity: 0.3;
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+`;
+export const IntroCharBox = styled.div`
+	position: absolute;
+	bottom: 0;
+	display: flex;
+	justify-content: space-around;
+	gap: 200px;
+	width: 100%;
+	height: max-content;
+
+	@media (max-width: 768px) {
+		gap: 40px;
+	}
+
+	img {
+		width: 100%;
+		max-width: 500px;
+		height: 100%;
+	}
+`;
+export const IntroLeftCharBox = styled.div`
+	animation: upDownLeft 3.5s infinite ease-in-out;
+
+	@keyframes upDownLeft {
+		0%,
+		100% {
+			transform: translateY(0);
+		}
+
+		50% {
+			transform: translateY(-10px);
+		}
+	}
+`;
+
+export const IntroRightCharBox = styled.div`
+	animation: upDownRight 3.5s infinite ease-in-out;
+
+	@keyframes upDownRight {
+		0%,
+		100% {
+			transform: translateY(10px);
+		}
+
+		50% {
+			transform: translateY(0);
+		}
+	}
+`;
+
+export const IntroContentBox = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	z-index: 1;
+`;
+export const IntroContent = styled.span``;
--- src/components/Main/Intro/Intro.tsx
+++ src/components/Main/Intro/Intro.tsx
@@ -1,5 +1,56 @@
-import { IntroContainer } from "./Intro.styled";
+import Image from "next/image";
+import {
+	IntroBackground,
+	IntroCharBox,
+	IntroContainer,
+	IntroContent,
+	IntroContentBox,
+	IntroLeftCharBox,
+	IntroLogo,
+	IntroRightCharBox,
+	IntroTitleBox,
+} from "./Intro.styled";
 
 export const Intro = () => {
-	return <IntroContainer>이곳은 메인 인트로</IntroContainer>;
+	return (
+		<IntroContainer>
+			<IntroTitleBox>
+				<IntroLogo>
+					<Image
+						src={"/logo.png"}
+						alt={"logo"}
+						width={550}
+						height={295}
+					/>
+				</IntroLogo>
+				<IntroContentBox>
+					<IntroContent>Content Text</IntroContent>
+					<IntroContent>Content Text</IntroContent>
+					<IntroContent>Content Text</IntroContent>
+					<IntroContent>Content Text</IntroContent>
+				</IntroContentBox>
+			</IntroTitleBox>
+			<IntroBackground>
+				<Image src="/title.png" alt="bg_intro" fill priority />
+			</IntroBackground>
+			<IntroCharBox>
+				<IntroLeftCharBox>
+					<Image
+						src="/duo.png"
+						alt="left_char"
+						width={500}
+						height={500}
+					/>
+				</IntroLeftCharBox>
+				<IntroRightCharBox>
+					<Image
+						src="/solo.png"
+						alt="right_char"
+						width={500}
+						height={500}
+					/>
+				</IntroRightCharBox>
+			</IntroCharBox>
+		</IntroContainer>
+	);
 };
