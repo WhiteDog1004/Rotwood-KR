52c7733898fdbcb261b84407fc4e0003b472ca61
WhiteDog <baek_dog@naver.com>
2024-05-31 21:54:02 +0900

    rw-13 feat : title 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,14 @@ import { Intro } from "@/components/Main";
 import { Faq } from "@/components/Main/Faq";
 import { Photo } from "@/components/Main/Photo";
 import { Translation } from "@/components/Main/Translation";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>라두두 한글패치</title>
+      </Head>
       <Intro />
       <Photo />
       <Translation />
